5956ca7b85109f94c40fac9e9752035c0e0b3c0d
VZGLY <chaineux.gavin@gmail.com>
2025-11-25 14:27:01 +0100

    v1.0.1

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ import Express from 'express'
 const app = Express()
 
 app.get('/', (_, res) => {
-    res.json({value: process.env.SECRET})
+    res.json({
+        value: process.env.SECRET,
+        value2: "Bonjour Deploy"
+    })
 })
 
 app.listen(3000);
